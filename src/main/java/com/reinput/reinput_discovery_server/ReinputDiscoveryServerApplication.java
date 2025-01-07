d3cee6a98c8ad8bed4b495d8813791f8fb0c236e
MinhoJJang <ahchjang@naver.com>
2025-01-07 18:36:44 +0900

    init: Initial project setup

---
+++ src/main/java/com/reinput/reinput_discovery_server/ReinputDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.reinput.reinput_discovery_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReinputDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReinputDiscoveryServerApplication.class, args);
+	}
+
+}
